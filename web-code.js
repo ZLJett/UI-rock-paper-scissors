acd4be19e71e6335e28bcb6474acec756c8ac41b
ZLJett <oneredraven@gmail.com>
2022-06-14 14:21:34 -0700

    Set playGame counters and start on core play loop

--- web-code.js
+++ web-code.js
@@ -1,11 +1,22 @@
 function playGame() {
+  let playerScore = 0;
+  let computerScore = 0;
+  let currentRound = 1;
 
+  // replace with WHILE LOOP = score less than 5
+  // play rounds until one player reaches 5 points 
+  for (let i = 0; i < 5; i++) {
+    // set up game round
+    const roundCounter = document.querySelector(".current-round");
+    roundCounter.textContent = currentRound;
+    displayValue.textContent = "Make your Selection";
+
+  }
 
 }
 
 
 function startGame(event) {
-  const displayValue = document.querySelector(".display-content");
   displayValue.textContent = "";
   startButton.classList.replace("styled", "greyout")
   startButton.removeEventListener("click", startGame);
@@ -14,6 +25,6 @@ function startGame(event) {
   console.log("StartGame End")
 }
 
-
+const displayValue = document.querySelector(".display-content");
 const startButton = document.querySelector(".game-button");
 startButton.addEventListener("click", startGame);
