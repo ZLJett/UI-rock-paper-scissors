7478943f22b1ea236db89c7b1e21c779e6874cb5
ZLJett <oneredraven@gmail.com>
2022-06-14 17:35:47 -0700

    Set up button input capture and input button's greyout addition and removal

--- web-code.js
+++ web-code.js
@@ -2,14 +2,21 @@ function playGame() {
   let playerScore = 0;
   let computerScore = 0;
   let currentRound = 1;
-
-  // replace with WHILE LOOP = score less than 5
-  // play rounds until one player reaches 5 points 
-  for (let i = 0; i < 5; i++) {
+  // play rounds until one player reaches 5 points
+  while (playerScore < 5 && computerScore < 5) {
     // set up game round
     const roundCounter = document.querySelector(".current-round");
+    const inputButtons = document.querySelectorAll(".pick-button");
+    let playerSelection = "";
     roundCounter.textContent = currentRound;
     displayValue.textContent = "Make your Selection";
+    inputButtons.forEach(button => button.classList.replace("greyout", "styled"));
+    // get player input
+    inputButtons.forEach(button => button.addEventListener("click", event => {
+      playerSelection = event.textContent;
+// TODO: ADD GREYOUT ONCE CAPTURE HAPPENS
+      console.log(playerSelection);
+    }, { once: true }));
 
   }
 
@@ -20,7 +27,7 @@ function startGame(event) {
   displayValue.textContent = "";
   startButton.classList.replace("styled", "greyout")
   startButton.removeEventListener("click", startGame);
-  let gameResult = playGame();
+  // let gameResult = playGame();
 
   console.log("StartGame End")
 }
