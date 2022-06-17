83d958a36d1f45a30bc4b59da486be92a560ce53
ZLJett <oneredraven@gmail.com>
2022-06-17 11:59:47 -0700

    Set event listener for start button and add outline of start position of web page

--- code-refactor2.js
+++ code-refactor2.js
@@ -2,3 +2,22 @@ let playerScore = 0;
 let computerScore = 0;
 let currentRound = 1;
 
+// web page loads in at "start position"
+// 1. display box starts as "Would you like to play?"
+// 2. all game info counters start at 0
+// 3. all choice buttons start with greyout class
+
+
+
+
+
+
+
+
+
+
+
+
+// start game event switched to "play round position"
+const startButton = document.querySelector(".game-button");
+startButton.addEventListener("click", playGame);
