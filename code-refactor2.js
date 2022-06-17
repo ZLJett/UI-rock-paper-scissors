94d7ff3c59fd0cdaec15498bc48598a053005912
ZLJett <oneredraven@gmail.com>
2022-06-17 14:38:27 -0700

    Remove setUpGame function as it is superfluous and add rest of game setup to startGame function

--- code-refactor2.js
+++ code-refactor2.js
@@ -1,3 +1,7 @@
+let playerScore = 0;
+let computerScore = 0;
+let currentRound = 1;
+
 // web page loads in at "start position" and thus this code works
 // of the assumption of these basic facts
 // 1. display box starts as "Would you like to play?"
@@ -8,24 +12,31 @@ function delay(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+function playRound (event) {
+
+
+
+  // if either score === 5 alert(winner) and run endGame
+  // else leave state as is, but set display back to make selection
+  // and update round counter (nextRound function)
+}
+
 function setUpGame() {
-  displayValue.textContent = "";
-  startButton.classList.replace("styled", "greyout")
-  startButton.removeEventListener("click", startGame);
+
+
 }
 
 function startGame() {
-  let playerScore = 0;   // move!!!!!!!!!!
-  let computerScore = 0;
-  let currentRound = 1;
-  setUpGame()
-  // idea here is that this listener is now just active, and is
-  // responsible for ending / removing itself and resetting everything
+  roundCounter.textContent = currentRound;
+  displayValue.textContent = "Make your Selection";
+  startButton.classList.replace("styled", "greyout")
+  startButton.removeEventListener("click", startGame);
+  inputButtons.forEach(button => button.classList.replace("greyout", "styled"));
   inputButtons.forEach(button => button.addEventListener("click", playRound));
 }
 
 
-
+const roundCounter = document.querySelector(".current-round");
 const inputButtons = document.querySelectorAll(".pick-button");
 const displayValue = document.querySelector(".display-content");
 
