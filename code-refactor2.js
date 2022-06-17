c6caba0057f0898e8a0f65fd241b43c9c9b30465
ZLJett <oneredraven@gmail.com>
2022-06-17 14:24:09 -0700

    Add startGame function that activates the choice buttons allowing the game to be played

--- code-refactor2.js
+++ code-refactor2.js
@@ -1,23 +1,34 @@
-let playerScore = 0;
-let computerScore = 0;
-let currentRound = 1;
-
-// web page loads in at "start position"
+// web page loads in at "start position" and thus this code works
+// of the assumption of these basic facts
 // 1. display box starts as "Would you like to play?"
 // 2. all game info counters start at 0
 // 3. all choice buttons start with greyout class
 
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function setUpGame() {
+  displayValue.textContent = "";
+  startButton.classList.replace("styled", "greyout")
+  startButton.removeEventListener("click", startGame);
+}
+
+function startGame() {
+  let playerScore = 0;   // move!!!!!!!!!!
+  let computerScore = 0;
+  let currentRound = 1;
+  setUpGame()
+  // idea here is that this listener is now just active, and is
+  // responsible for ending / removing itself and resetting everything
+  inputButtons.forEach(button => button.addEventListener("click", playRound));
+}
 
 
 
-
-
-
-
-
-
-
+const inputButtons = document.querySelectorAll(".pick-button");
+const displayValue = document.querySelector(".display-content");
 
 // start game event switched to "play round position"
 const startButton = document.querySelector(".game-button");
-startButton.addEventListener("click", playGame);
+startButton.addEventListener("click", startGame);
